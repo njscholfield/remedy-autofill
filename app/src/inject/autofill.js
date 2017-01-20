b88d6e6362ec7a74a15d178942a5f0cacf42d635
Noah Scholfield <contact@noahscholfield.com>
2017-01-20 00:30:47 -0500

    Check if in search mode, allow enable/disable features

--- app/src/inject/autofill.js
+++ app/src/inject/autofill.js
@@ -1,15 +1,27 @@
+/* eslint no-unused-vars: 0 */
 /* global chrome */
 
 (function() {
-  let username = undefined;
-  let assignment = undefined;
+  let settings = {username: undefined, assignment: undefined, options: []};
 
-  chrome.storage.sync.get(['location', 'username'], function(items) {
-    if(items.location === undefined || items.username === undefined || items.username === '') {
+  var listeners = [
+    'templateTitle.addEventListener(\'dblclick\', template)',
+    'category.addEventListener(\'dblclick\', rest)',
+    'custUsername.addEventListener(\'keypress\', (e) => (e.key === \'Enter\') ? phnum.focus(): false)',
+    'phnum.addEventListener(\'keypress\', (e) => (e.key === \'Enter\') ? apptType.focus() : false)',
+    'apptType.addEventListener(\'change\', () => osType.focus())',
+    'solutionType.addEventListener(\'dblclick\', closeTicket)',
+    'solutionType.addEventListener(\'change\', () => hoursWorked.focus())'
+  ];
+
+  chrome.storage.sync.get(['location', 'username', 'options'], function(items) {
+    if(!items.location || !items.username) {
       alert('If you want to use Remedy Autofill, you have to configure it in extension settings.');
     } else {
-      username = items.username;
-      assignment = items.location;
+      settings.username = items.username;
+      settings.assignment = items.location;
+      settings.options = items.options;
+      applySettings(listeners);
     }
   });
 
@@ -27,13 +39,15 @@
   const closed = document.querySelector('#arid_WIN_0_536870980');
   const solutionType = document.querySelector('#arid_WIN_0_536870958');
   const hoursWorked = document.querySelector('#arid_WIN_0_536870933');
+  const remedyMode = document.querySelector('.TBTopBarStatusMode');
 
   const change = new Event('change');
 
   function template() {
+    checkRemedyMode();
     templateCategory.value = 'SCS';
     templateCategory.dispatchEvent(change);
-    templateTitle.value = `SCS: Walk-in ${assignment}`;
+    templateTitle.value = `SCS: Walk-in ${settings.assignment}`;
     templateTitle.dispatchEvent(change);
   }
 
@@ -42,7 +56,7 @@
     category.dispatchEvent(change);
     subCategory.value = 'Software Application - Supported';
     subCategory.dispatchEvent(change);
-    assignedTo.value = username;
+    assignedTo.value = settings.username;
     assignedTo.dispatchEvent(change);
     longDescription.value = longDescription.value.replace(', Desktop, Tablet, Smartphone', '');
     custUsername.focus();
@@ -56,11 +70,15 @@
     solutionType.focus();
   }
 
-  templateTitle.addEventListener('dblclick', template);
-  category.addEventListener('dblclick', rest);
-  custUsername.addEventListener('keypress', (e) => (e.key === 'Enter') ? phnum.focus(): false);
-  phnum.addEventListener('keypress', (e) => (e.key === 'Enter') ? apptType.focus() : false);
-  apptType.addEventListener('change', () => osType.focus());
-  solutionType.addEventListener('dblclick', closeTicket);
-  solutionType.addEventListener('change', () => hoursWorked.focus());
+  function checkRemedyMode() {
+    if(remedyMode.innerHTML === 'Search') {
+      alert('You are in search mode. Are you sure that is what you want?');
+    }
+  }
+
+  function applySettings(allListeners) {
+    var usedListeners = allListeners.filter((item, index) => settings.options[index]);
+    usedListeners.forEach((listener) => eval(listener));
+  }
+
 })();
--- app/src/options/options.js
+++ app/src/options/options.js
@@ -5,20 +5,26 @@
   const assignment = document.querySelector('#location');
   const form = document.querySelector('form');
   const success = document.querySelector('.success');
+  const features = [...document.querySelectorAll('input[type="checkbox"]')];
 
   function submit(e) {
     e.preventDefault();
     const userval = user.value;
     const assignVal = assignment.value;
-    chrome.storage.sync.set({username: userval, location: assignVal}, function() {
+    const options = features.map((input, index, array) => {
+      return (index === 3 || index === 4) ? array[2].checked : (index === 6) ? array[5].checked : input.checked;
+    });
+
+    chrome.storage.sync.set({username: userval, location: assignVal, options: options}, function() {
       success.classList.remove('hidden');
     });
   }
 
   (function fillSavedValues() {
-    chrome.storage.sync.get(['username', 'location'], function(items) {
-      user.value = items.username || '';
-      assignment.value = items.location || 'TechRow';
+    chrome.storage.sync.get(['username', 'location', 'options'], function(settings) {
+      user.value = settings.username || '';
+      assignment.value = settings.location || 'TechRow';
+      features.forEach((checkbox, index) => checkbox.checked = settings.options[index]);
     });
   })();
 
