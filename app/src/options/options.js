9eb0984462c9997644b85f2878778b5ba06b6111
Noah Scholfield <contact@noahscholfield.com>
2018-03-25 16:03:19 -0400

    Renamed variable from `assignment` to `location`

--- app/src/options/options.js
+++ app/src/options/options.js
@@ -2,7 +2,7 @@
 /* Created by Noah Scholfield */
 
 (function(){
-  const assignment = document.querySelector('#location');
+  const location = document.querySelector('#location');
   const form = document.querySelector('form');
   const success = document.querySelector('.success');
   const features = [...document.querySelectorAll('input[type="checkbox"]')];
@@ -10,7 +10,7 @@
   // Saves extension settings
   function submit(e) {
     e.preventDefault();
-    const assignVal = assignment.value;
+    const assignVal = location.value;
     const options = features.map((input) => {
       return input.checked;
     });
@@ -20,10 +20,10 @@
     });
   }
 
-  // Displays the currently saves settings
+  // Displays the currently saved settings
   (function fillSavedValues() {
     chrome.storage.sync.get(['location', 'options'], function(settings) {
-      assignment.value = settings.location || 'University_Store_on_5th';
+      location.value = settings.location || 'University_Store_on_5th';
       features.forEach((checkbox, index) => checkbox.checked = settings.options[index]);
     });
   })();
